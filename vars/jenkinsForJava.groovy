398fc53150d12554c36a4420b5d6920584521415
mahesh430 <umamahesh690@gmail.com>
2021-11-03 12:48:47 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -1,30 +1,38 @@
-def call(String repoUrl){
+def call(String repoUrl) {
   pipeline {
-    agent any
-    stages{
-      
-//       stage('Tools initilization')
-//       {
-//         steps{
-//         sh "mvn --version"
-//         sh "mvn --version"
-//         }
-//       }
-      stage('Code checkout'){
-        steps{
-//             echo 'Pulling...' + env.BRANCH_NAME
-
-         git branch: 'master'
-          echo "repo name is--"+"${repoUrl}"
-         url: "${repoUrl}"
-        }
-        
-      }
-      
-      
-    }
-    
-    
-  }
-  
+       agent any
+       tools {
+           maven 'Maven 3.5.0'
+           jdk 'jdk8'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+   }
 }
